7dd42f8826f6edb98c80fd3d8fcd725cf7e8c5f2
Barath D <79625824+barath02@users.noreply.github.com>
2021-08-30 21:33:58 +0530

    Initial commit

---
+++ src/course/course.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param, Post, Body, Delete, Query} from '@nestjs/common';
+import { CourseService } from './course.service';
+import { CreateCourseDto } from './create-course.dto';
+
+@Controller('course')
+export class CourseController {
+    constructor(private courseService: CourseService) {}
+
+    @Get()
+    async getCourses() {
+        const course = await this.courseService.getCourses();
+        return course;
+    }
+
+    @Get(':courseId')
+    async getCourse(@Param('courseId') courseId) {
+        const course = await this.courseService.getCourse(courseId);
+        return course;
+    }
+
+    @Post()
+    async addCourse(@Body() createCourseDto: CreateCourseDto) {
+        const course = await this.courseService.addCourse(createCourseDto);
+        return course;
+    }
+
+    @Delete()
+    async deleteCourse(@Query() query) {
+        const course = await this.courseService.deleteCourse(query.courseId);
+        return course;
+    }
+}
